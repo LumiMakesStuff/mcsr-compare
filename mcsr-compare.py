8b9fc5e2cc1279f7257c8db6f240edf96daea0fb
DucksAreVeryCoolDev <lucasgrant2020@outlook.com>
2026-05-30 13:35:13 +0100

    Add status check and endpoint selection
    Endpoints found on https://docs.mcsrranked.com/

--- mcsr-compare.py
+++ mcsr-compare.py
@@ -1,2 +1,45 @@
 # Import
 import requests
+import json
+
+# Variables
+MCSR_ENDPOINT = "https://api.mcsrranked.com/"
+MCSR_ENDPOINT_ALT = "https://mcsrranked.com/api/"
+
+chosenEndpoint = ""
+
+# Function
+def check_status():
+    print("Don't forget all MCSR Ranked endpoints have a limit of 500 requests per minute unless specified on their documentation.")
+
+    print()
+
+    print(f"Checking endpoint {MCSR_ENDPOINT}...")
+    mainEndpointResponse = requests.get(MCSR_ENDPOINT)
+
+    if mainEndpointResponse.status_code == 200:
+        print("Recieved code 200. Using main endpoint.")
+        chosenEndpoint = MCSR_ENDPOINT
+    else:
+        Warning(f"Main endpoint failed with code {mainEndpointResponse.status_code}.")
+        
+        print(f"Checking endpoint {MCSR_ENDPOINT_ALT}...")
+        altEndpointResponse = requests.get(MCSR_ENDPOINT_ALT)
+
+        if altEndpointResponse.status_code == 200:
+            print("Recieved code 200. Using alternate endpoint")
+            chosenEndpoint = altEndpointResponse
+
+def get_runner_data():
+    pass
+
+# Welcome
+print("Welcome to mcsr-compare!")
+print()
+
+# Endpoint check
+check_status()
+
+
+
+input("Press enter to finish.")
